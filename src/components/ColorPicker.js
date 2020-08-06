2a9f42c723c6d1d967d253f1027d6637b981ef75
Vlad Shilov <omgovich@ya.ru>
2020-08-06 21:55:35 +0300

    Update local HSV if `hex` property value is changed

--- src/components/ColorPicker.js
+++ src/components/ColorPicker.js
@@ -1,25 +1,37 @@
-import React, { useState, useEffect, useCallback } from "react";
+import React, { useState, useEffect, useCallback, useRef } from "react";
 import Hue from "./Hue";
 import Saturation from "./Saturation";
 import { hsvToHex, hexToHsv, formatClassName } from "../utils";
 import styles from "../styles.css";
 
 const ColorPicker = ({ className, hex, onChange }) => {
-  // Input and output formats are HEX (#AAAAAA),
+  // Input and output formats are HEX (#aabbcc),
   // but all internal calculations are based on HSV model
   const [hsv, updateHsv] = useState(() => hexToHsv(hex));
 
-  // Convert updated HSV to HEX-format and send it to the parent component
+  // By using this ref we're able to prevent extra updates
+  // and the effects recursion during the color conversion
+  const sourceHexRef = useRef(hex);
+
+  // Update local HSV if `hex` property value is changed,
+  // but only if it's not the same HEX-color that we just sent to the parent
   useEffect(() => {
-    onChange(hsvToHex(hsv));
+    if (hex !== sourceHexRef.current) updateHsv(hexToHsv(hex));
+  }, [hex]);
+
+  // Convert updated HSV to HEX-format, send it to the parent component
+  // and save the new HEX-color to the ref to prevent an unnecessary update
+  useEffect(() => {
+    const newHex = hsvToHex(hsv);
+    sourceHexRef.current = newHex;
+    onChange(newHex);
   }, [hsv, onChange]);
 
   // Merge the current HSV color object with updated params.
   // For example, when a child component sends `hue` or `sat` only
-  const handleChange = useCallback(
-    (params) => updateHsv((current) => Object.assign({}, current, params)),
-    []
-  );
+  const handleChange = useCallback((params) => {
+    updateHsv((current) => Object.assign({}, current, params));
+  }, []);
 
   const nodeClassName = formatClassName(["react-colorful", styles.container, className]);
 
