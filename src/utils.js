7297e0a7490ddd6852326da1802e54121c5886e9
Vlad Shilov <omgovich@ya.ru>
2020-08-07 12:10:57 +0300

    Migrate to the latest version of @swiftcarrot/color-fns

--- src/utils.js
+++ src/utils.js
@@ -1,16 +1,11 @@
-import { hex2rgb, rgb2hsv, hsv2rgb, rgb2hex } from "@swiftcarrot/color-fns";
-
-console.log(hsv2rgb({ h: 360, s: 50, v: 50 }));
+import { hex2rgb, rgb2hsv, hsv2hex } from "@swiftcarrot/color-fns";
 
 export const hexToHsv = (hex) => {
   const { r, g, b } = hex2rgb(hex);
   return rgb2hsv(r, g, b);
 };
 
-export const hsvToHex = ({ h, s, v }) => {
-  const { r, g, b } = hsv2rgb(h, s, v);
-  return rgb2hex(r, g, b);
-};
+export const hsvToHex = ({ h, s, v }) => hsv2hex(h, s, v);
 
 export const limit = (number, min = 0, max = 1) => Math.min(Math.max(min, number), max);
 
