00866d73d438e115b272d4746c7725cb5858901c
Vlad Shilov <omgovich@ya.ru>
2020-08-12 12:09:20 +0300

    Write tests with Jest and React Testing Library
    Uninstall uvu test runner

--- tests/components.test.js
+++ tests/components.test.js
@@ -1,30 +1,54 @@
 import React from "react";
-import { render, cleanup, fireEvent } from "@testing-library/react";
+import { render, cleanup, fireEvent, waitFor } from "@testing-library/react";
 import ColorPicker from "../src/";
 
 afterEach(cleanup);
 
-it("renders proper HTML", () => {
-  const result = render(<ColorPicker hex="#000" />);
-  const container = result.container.firstChild;
+// Fix to pass `pageX` and `pageY`
+// See https://github.com/testing-library/react-testing-library/issues/268
+class FakeMouseEvent extends MouseEvent {
+  constructor(type, values = {}) {
+    const { pageX, pageY, ...rest } = values;
+    super(type, rest);
 
-  expect(container).toMatchSnapshot();
+    Object.assign(this, {
+      pageX: pageX || 0,
+      pageY: pageY || 0,
+    });
+  }
+}
+
+it("Renders proper HTML", () => {
+  const result = render(<ColorPicker hex="#F00" />);
+
+  expect(result.container.firstChild).toMatchSnapshot();
 });
 
-it("doesn't trigger onChange after mounting", () => {
-  const handleChange = jest.fn(() => true);
+it("Doesn't trigger `onChange` after mounting", () => {
+  const handleChange = jest.fn();
   render(<ColorPicker onChange={handleChange} />);
 
   expect(handleChange).not.toHaveReturned();
 });
 
-it("triggers onChange after clicking", () => {
-  const handleChange = jest.fn(() => true);
+it("Triggers `onChange` after a mouse interaction", async () => {
+  const handleChange = jest.fn();
   const result = render(<ColorPicker onChange={handleChange} />);
-  const saturation = result.container.querySelector(".react-colorful__saturation");
+  const saturation = result.container.querySelector(".react-colorful__saturation .interactive");
 
-  fireEvent(saturation.firstChild, new MouseEvent("mousedown", { pageX: 0, pageY: 0 }));
-  fireEvent(saturation.firstChild, new MouseEvent("mousemove", { pageX: 50, pageY: 50 }));
+  fireEvent(saturation, new FakeMouseEvent("mousedown", { pageX: 0, pageY: 0, bubbles: true }));
+  fireEvent(saturation, new FakeMouseEvent("mousemove", { pageX: 10, pageY: 10, bubbles: true }));
+
+  expect(handleChange).toHaveReturned();
+});
+
+it("Triggers `onChange` after a touch interaction", async () => {
+  const handleChange = jest.fn();
+  const result = render(<ColorPicker onChange={handleChange} />);
+  const hue = result.container.querySelector(".react-colorful__hue .interactive");
+
+  fireEvent.touchStart(hue, { touches: [{ pageX: 0, pageY: 0, bubbles: true }] });
+  fireEvent.touchMove(hue, { touches: [{ pageX: 100, pageY: 0, bubbles: true }] });
 
   expect(handleChange).toHaveReturned();
 });
