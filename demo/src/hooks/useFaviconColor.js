795b126bd943bbaa535ae3592f7c13d2865a2353
Vlad Shilov <omgovich@ya.ru>
2020-08-13 22:08:30 +0300

    Improve useFaviconColor performance

--- demo/src/hooks/useFaviconColor.js
+++ demo/src/hooks/useFaviconColor.js
@@ -1,56 +1,87 @@
-import { useRef, useEffect } from "react";
+import { useRef, useCallback } from "react";
+import useThrottledEffect from "use-throttled-effect";
+
+const ICON_SIZE = 64;
+const OUTLINE_RADIUS = 28;
+const POINTER_RADIUS = 22;
+
+const createCanvas = () => {
+  const canvas = document.createElement("canvas");
+  canvas.width = ICON_SIZE;
+  canvas.height = ICON_SIZE;
+  return canvas;
+};
+
+const createBackgroundCanvas = () => {
+  const canvas = createCanvas();
+  const ctx = canvas.getContext("2d");
+
+  ctx.beginPath();
+  ctx.arc(ICON_SIZE * 0.5, ICON_SIZE * 0.5, OUTLINE_RADIUS, 0, 2 * Math.PI, false);
+  ctx.closePath();
+
+  ctx.shadowColor = "rgba(0, 0, 0, 0.4)";
+  ctx.shadowOffsetY = 1;
+  ctx.shadowBlur = 6;
+  ctx.fillStyle = "#fff";
+  ctx.fill();
+
+  return canvas;
+};
 
 const useFaviconColor = (color) => {
+  const faviconNode = useRef();
   const canvas = useRef();
+  const backgroundCanvas = useRef();
 
-  const favSize = 64;
-  const outlineRadius = 28;
-  const pointerRadius = 22;
-
-  // init canvas only once
+  // create canvases only once
   if (!canvas.current) {
-    canvas.current = document.createElement("canvas");
-    canvas.current.width = favSize;
-    canvas.current.height = favSize;
+    canvas.current = createCanvas();
+    backgroundCanvas.current = createBackgroundCanvas(); // draw the background once
   }
 
-  useEffect(() => {
+  // generate a favicon only once on mobiles in order to improve performance
+  const shouldReplace = () => {
+    if (window.innerWidth < 768 && !faviconNode.current) return false;
+    return true;
+  };
+
+  // draw a new favicon and replace `link` tag
+  const replaceFavicon = (pointerColor) => {
     const ctx = canvas.current.getContext("2d");
 
     // wipe out the canvas
-    ctx.clearRect(0, 0, favSize, favSize);
+    ctx.clearRect(0, 0, ICON_SIZE, ICON_SIZE);
 
-    // draw an outline with shadow
-    ctx.save();
-    ctx.beginPath();
-    ctx.arc(favSize * 0.5, favSize * 0.5, outlineRadius, 0, 2 * Math.PI, false);
-    ctx.closePath();
-
-    ctx.shadowColor = "rgba(0, 0, 0, 0.4)";
-    ctx.shadowOffsetY = 1;
-    ctx.shadowBlur = 6;
-    ctx.fillStyle = "#fff";
-    ctx.fill();
-    ctx.restore();
+    // draw the cached background
+    ctx.drawImage(backgroundCanvas.current, 0, 0);
 
     // draw a pointer
     ctx.beginPath();
-    ctx.arc(favSize * 0.5, favSize * 0.5, pointerRadius, 0, 2 * Math.PI, false);
+    ctx.arc(ICON_SIZE * 0.5, ICON_SIZE * 0.5, POINTER_RADIUS, 0, 2 * Math.PI, false);
     ctx.closePath();
-    ctx.fillStyle = color;
+    ctx.fillStyle = pointerColor;
     ctx.fill();
 
+    // create a new favicon tag
     const link = document.createElement("link");
-    link.id = "fav-pointer";
     link.rel = "shortcut icon";
     link.href = canvas.current.toDataURL("image/x-icon");
 
     // remove the old favicon from the document
-    const prevFavicon = document.getElementById("fav-pointer");
-    if (prevFavicon) document.head.removeChild(prevFavicon);
+    if (faviconNode.current) document.head.removeChild(faviconNode.current);
 
     document.head.appendChild(link);
-  }, [color]);
+    faviconNode.current = link;
+  };
+
+  useThrottledEffect(
+    () => {
+      if (shouldReplace()) replaceFavicon(color);
+    },
+    500,
+    [color]
+  );
 };
 
 export default useFaviconColor;
