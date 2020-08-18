54c93d4d1d0d25af8f66c1b562f958f4303547ff
Vlad Shilov <omgovich@ya.ru>
2020-08-19 00:14:50 +0300

    Rewrite `rgbToHsv` to make it lighter

--- src/utils/rgbToHsv.js
+++ src/utils/rgbToHsv.js
@@ -1,26 +1,20 @@
 const rgbToHsv = ({ r, g, b }) => {
-  let h, s, v;
   let max = Math.max(r, g, b);
-  let min = Math.min(r, g, b);
-  let delta = max - min;
+  let delta = max - Math.min(r, g, b);
 
-  if (delta === 0) {
-    h = 0;
-  } else if (r === max) {
-    h = ((g - b) / delta) % 6;
-  } else if (g === max) {
-    h = (b - r) / delta + 2;
-  } else {
-    h = (r - g) / delta + 4;
-  }
+  let hh = delta
+    ? max === r
+      ? (g - b) / delta
+      : max === g
+      ? 2 + (b - r) / delta
+      : 4 + (r - g) / delta
+    : 0;
 
-  h = Math.round(h * 60);
-  s = Math.round((max === 0 ? 0 : delta / max) * 100);
-  v = Math.round((max / 255) * 100);
-
-  if (h < 0) h += 360;
-
-  return { h, s, v };
+  return {
+    h: Math.round(60 * (hh < 0 ? hh + 6 : hh)),
+    s: Math.round(max ? (delta / max) * 100 : 0),
+    v: Math.round((max / 255) * 100),
+  };
 };
 
 export default rgbToHsv;
