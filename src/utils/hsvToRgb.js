c432407029e1c2856245ec595a660cb8965d407e
Vlad Shilov <omgovich@ya.ru>
2020-08-19 11:57:53 +0300

    Make `hsvToRgb` lighter

--- src/utils/hsvToRgb.js
+++ src/utils/hsvToRgb.js
@@ -3,17 +3,17 @@ const hsvToRgb = ({ h, s, v }) => {
   s = s / 100;
   v = v / 100;
 
-  let i = Math.floor(h),
-    f = h - i,
-    p = v * (1 - s),
-    q = v * (1 - f * s),
-    t = v * (1 - (1 - f) * s),
-    mod = i % 6,
-    r = Math.round([v, q, p, p, t, v][mod] * 255),
-    g = Math.round([t, v, v, q, p, p][mod] * 255),
-    b = Math.round([p, p, t, v, v, q][mod] * 255);
+  let hh = Math.floor(h),
+    b = v * (1 - s),
+    c = v * (1 - (h - hh) * s),
+    d = v * (1 - (1 - h - hh) * s),
+    module = hh % 6;
 
-  return { r, g, b };
+  return {
+    r: Math.round([v, c, b, b, d, v][module] * 255),
+    g: Math.round([d, v, v, c, b, b][module] * 255),
+    b: Math.round([b, b, d, v, v, c][module] * 255),
+  };
 };
 
 export default hsvToRgb;
