97032fcbd13d37f10671442ac731e6a6b12a7575
Ryan Christian <ryanchristian4427@gmail.com>
2020-08-29 20:20:01 +0100

    docs: Adding comment to explain color object casting

--- src/utils/compare.ts
+++ src/utils/compare.ts
@@ -2,15 +2,19 @@ import { hexToRgb } from "./convert";
 
 import { HSL, HSV, RGB } from "../types";
 
-interface ColorCompare {
-  [key: string]: number;
-}
-
 export const equalColorObjects = (first: HSL | HSV | RGB, second: HSL | HSV | RGB): boolean => {
   if (first === second) return true;
 
   for (const prop in first) {
-    if (((first as unknown) as ColorCompare)[prop] !== ((second as unknown) as ColorCompare)[prop])
+    // The following allows for a type-safe calling of this function (first & second have to be HSL, HSV, or RGB)
+    // with type-unsafe iterating over object keys. TS does not allow this without an index (`[key: string]: number`)
+    // on an object to define how iteration is normally done. To ensure extra keys are not allowed on our types,
+    // we must cast our object to unknown (as RGB demands `r` be a key, while `Record<string, x>` does not care if
+    // there is or not), and then as a type TS can iterate over.
+    if (
+      ((first as unknown) as Record<string, number>)[prop] !==
+      ((second as unknown) as Record<string, number>)[prop]
+    )
       return false;
   }
 
