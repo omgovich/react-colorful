29066932bd558d12b52eed8478e36539d7b95717
Vlad Shilov <omgovich@ya.ru>
2020-09-02 21:20:29 +0300

    Add `useEventCallback` hook to `HexInput`

--- demo/src/index.js
+++ demo/src/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback } from "react";
+import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom";
 import ColorPicker from "../../src/hex";
 import HexInput from "../../src/HexInput";
