99f90f1b446605896a72db0546a0dc95a03018fb
Ryan Christian <ryanchristian4427@gmail.com>
2020-09-02 20:03:50 +0100

    refactor: Adding common export / updating build script

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+import HexColorPicker from "./packages/hex";
+import HexInput from "./packages/HexInput";
+import HslColorPicker from "./packages/hsl";
+import HslStringColorPicker from "./packages/hslString";
+import HsvColorPicker from "./packages/hsv";
+import RgbColorPicker from "./packages/rgb";
+import RgbStringColorPicker from "./packages/rgbString";
+
+export {
+  HexColorPicker,
+  HexInput,
+  HslColorPicker,
+  HslStringColorPicker,
+  HsvColorPicker,
+  RgbColorPicker,
+  RgbStringColorPicker,
+};
