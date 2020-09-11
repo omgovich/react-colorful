80b433dc873fbc7e9388d66f6e7dc633adb2e474
Vlad Shilov <omgovich@ya.ru>
2020-09-11 17:32:39 +0300

    Display all pickers on the demo page (dev only)

--- demo/src/index.tsx
+++ demo/src/index.tsx
@@ -1,6 +1,33 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom";
-import { HexColorPicker, HexColorInput, RgbaColor } from "../../src";
+import {
+  // HEX
+  HexColorPicker,
+  HexColorInput,
+  // RGB
+  RgbColor,
+  RgbColorPicker,
+  RgbStringColorPicker,
+  // RGBA
+  RgbaColor,
+  RgbaColorPicker,
+  RgbaStringColorPicker,
+  // HSL
+  HslColor,
+  HslColorPicker,
+  HslStringColorPicker,
+  // HSLA
+  HslaColor,
+  HslaColorPicker,
+  HslaStringColorPicker,
+  // HSV
+  HsvColor,
+  HsvColorPicker,
+  // HSVA
+  HsvaColor,
+  HsvaColorPicker,
+} from "../../src";
+import { PickerPreview } from "./components/PickerPreview";
 import { hexToRgba } from "../../src/utils/convert";
 import { useFaviconColor } from "./hooks/useFaviconColor";
 import { useBodyBackground } from "./hooks/useBodyBackground";
@@ -22,8 +49,8 @@ const Demo = () => {
   useFaviconColor(color);
 
   return (
-    <div className={styles.wrapper} style={{ color: textColor }}>
-      <header className={styles.header}>
+    <div>
+      <header className={styles.header} style={{ color: textColor }}>
         <div className={styles.demo}>
           <HexColorPicker className={styles.colorPicker} color={color} onChange={handleChange} />
           <div className={styles.field}>
@@ -45,6 +72,61 @@ const Demo = () => {
           </nav>
         </div>
       </header>
+
+      {process.env.NODE_ENV === "development" && (
+        <div>
+          <PickerPreview<RgbColor>
+            title="RGB"
+            PickerComponent={RgbColorPicker}
+            initialColor={{ r: 60, g: 80, b: 120 }}
+          />
+          <PickerPreview<string>
+            title="RGB String"
+            PickerComponent={RgbStringColorPicker}
+            initialColor="rgb(60, 80, 120)"
+          />
+          <PickerPreview<RgbaColor>
+            title="RGBA"
+            PickerComponent={RgbaColorPicker}
+            initialColor={{ r: 60, g: 80, b: 120, a: 0.5 }}
+          />
+          <PickerPreview<string>
+            title="RGBA String"
+            PickerComponent={RgbaStringColorPicker}
+            initialColor="rgba(60, 80, 120, 0.5)"
+          />
+          <PickerPreview<HslColor>
+            title="HSL"
+            PickerComponent={HslColorPicker}
+            initialColor={{ h: 200, s: 25, l: 32 }}
+          />
+          <PickerPreview<string>
+            title="HSL String"
+            PickerComponent={HslStringColorPicker}
+            initialColor="hsl(200, 25%, 32%)"
+          />
+          <PickerPreview<HslaColor>
+            title="HSLA"
+            PickerComponent={HslaColorPicker}
+            initialColor={{ h: 200, s: 25, l: 32, a: 0.5 }}
+          />
+          <PickerPreview<string>
+            title="HSLA String"
+            PickerComponent={HslaStringColorPicker}
+            initialColor="hsl(200, 25%, 32%, 0.5)"
+          />
+          <PickerPreview<HsvColor>
+            title="HSV"
+            PickerComponent={HsvColorPicker}
+            initialColor={{ h: 200, s: 25, v: 50 }}
+          />
+          <PickerPreview<HsvaColor>
+            title="HSVA"
+            PickerComponent={HsvaColorPicker}
+            initialColor={{ h: 200, s: 25, v: 50, a: 0.5 }}
+          />
+        </div>
+      )}
     </div>
   );
 };
