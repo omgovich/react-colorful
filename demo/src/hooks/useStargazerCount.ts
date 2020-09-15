79adba85652d78259f4c4e4ccec0d1859dbcadd4
Vlad Shilov <omgovich@ya.ru>
2020-09-15 16:43:16 +0300

    Display alpha picker on the demo page

--- demo/src/hooks/useStargazerCount.ts
+++ demo/src/hooks/useStargazerCount.ts
@@ -4,9 +4,11 @@ export const useStargazerCount = (): number => {
   const [count, setCount] = useState(300);
 
   useEffect(() => {
-    fetch("https://api.github.com/repos/omgovich/react-colorful").then((result) => {
-      result.json().then((data) => setCount(data.stargazers_count));
-    });
+    if (process.env.NODE_ENV !== "development") {
+      fetch("https://api.github.com/repos/omgovich/react-colorful").then((result) => {
+        result.json().then((data) => setCount(data.stargazers_count));
+      });
+    }
   }, []);
 
   return count;
