5fea82a9993e41118caad16caf326f5b76120c64
Vlad Shilov <omgovich@ya.ru>
2020-09-16 14:02:50 +0300

    Fix specs

--- src/components/common/Interactive.tsx
+++ src/components/common/Interactive.tsx
@@ -1,22 +1,12 @@
 import React, { useState, useLayoutEffect, useRef, useCallback } from "react";
 import styles from "../../css/styles.css";
 
-let hasTouched = false;
-
 // Limit number within [0, 1] bounds.
 // Use ternary operator instead of `Math.min(Math.max(0, number), 1)` to save few bytes
 const limit = (number: number) => (number > 1 ? 1 : number < 0 ? 0 : number);
 
 // Check if an event was triggered by touch
 const isTouch = (e: MouseEvent | TouchEvent) => window.TouchEvent && e instanceof TouchEvent;
-
-// Prevent mobile browsers from handling mouse events (conflicting with touch ones).
-// If we detected a touch interaction before, we prefer reacting to touch events only.
-const isValid = (event: MouseEvent | TouchEvent): boolean => {
-  if (hasTouched && !isTouch(event)) return false;
-  if (!hasTouched) hasTouched = isTouch(event);
-  return true;
-};
 
 export interface Interaction {
   left: number;
@@ -30,6 +20,7 @@ interface Props {
 
 const InteractiveBase = ({ onMove, children }: Props) => {
   const container = useRef<HTMLDivElement>(null);
+  const hasTouched = useRef(false);
   const [isDragging, setDragging] = useState(false);
 
   const getRelativePosition = useCallback((event: MouseEvent | TouchEvent) => {
@@ -45,6 +36,14 @@ const InteractiveBase = ({ onMove, children }: Props) => {
       top: limit((pointer.pageY - (rect.top + window.pageYOffset)) / rect.height),
     };
   }, []);
+
+  // Prevent mobile browsers from handling mouse events (conflicting with touch ones).
+  // If we detected a touch interaction before, we prefer reacting to touch events only.
+  const isValid = (event: MouseEvent | TouchEvent): boolean => {
+    if (hasTouched.current && !isTouch(event)) return false;
+    if (!hasTouched.current) hasTouched.current = isTouch(event);
+    return true;
+  };
 
   const handleMove = useCallback(
     (event: MouseEvent | TouchEvent) => {
@@ -72,8 +71,8 @@ const InteractiveBase = ({ onMove, children }: Props) => {
     (state) => {
       // add or remove additional pointer event listeners
       const toggleEvent = state ? window.addEventListener : window.removeEventListener;
-      toggleEvent(hasTouched ? "touchmove" : "mousemove", handleMove);
-      toggleEvent(hasTouched ? "touchend" : "mouseup", handleMoveEnd);
+      toggleEvent(hasTouched.current ? "touchmove" : "mousemove", handleMove);
+      toggleEvent(hasTouched.current ? "touchend" : "mouseup", handleMoveEnd);
     },
     [handleMove, handleMoveEnd]
   );
