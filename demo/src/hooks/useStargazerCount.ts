96924026284dbc94de9d448b9395e93d8dedd13e
Vlad Shilov <omgovich@ya.ru>
2020-09-25 23:42:51 +0300

    Make the demo working in IE11

--- demo/src/hooks/useStargazerCount.ts
+++ demo/src/hooks/useStargazerCount.ts
@@ -4,7 +4,7 @@ export const useStargazerCount = (): number => {
   const [count, setCount] = useState(300);
 
   useEffect(() => {
-    if (process.env.NODE_ENV !== "development") {
+    if (process.env.NODE_ENV !== "development" && "fetch" in window) {
       fetch("https://api.github.com/repos/omgovich/react-colorful").then((result) => {
         result.json().then((data) => setCount(data.stargazers_count));
       });
