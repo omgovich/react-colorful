95b067d6cb92b386093d077ba9631491203c8143
Vlad Shilov <omgovich@ya.ru>
2020-09-25 17:05:26 +0300

    Describe ternary operators usage

--- src/utils/clamp.ts
+++ src/utils/clamp.ts
@@ -1,5 +1,6 @@
 // Clamps a value between an upper and lower bound.
-// Used ternary operator instead of `Math.min(Math.max(min, number), max)` to save few bytes
+// We use ternary operators because it makes the minified code
+// 2 times shorter then `Math.min(Math.max(a,b),c)`
 export const clamp = (number: number, min = 0, max = 1): number => {
   return number > max ? max : number < min ? min : number;
 };
