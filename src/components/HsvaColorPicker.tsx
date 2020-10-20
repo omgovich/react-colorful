198979325889a8e8d0ff158f140ed187e29597e2
Vlad Shilov <omgovich@ya.ru>
2020-10-20 23:37:22 +0300

    Round ouput values

--- src/components/HsvaColorPicker.tsx
+++ src/components/HsvaColorPicker.tsx
@@ -3,11 +3,12 @@ import React from "react";
 import { AlphaColorPicker } from "./common/AlphaColorPicker";
 import { ColorModel, ColorPickerBaseProps, HsvaColor } from "../types";
 import { equalColorObjects } from "../utils/compare";
+import { roundHsva } from "../utils/convert";
 
 const colorModel: ColorModel<HsvaColor> = {
   defaultColor: { h: 0, s: 0, v: 0, a: 1 },
   toHsva: (hsva) => hsva,
-  fromHsva: (hsva) => hsva,
+  fromHsva: roundHsva,
   equal: equalColorObjects,
 };
 
