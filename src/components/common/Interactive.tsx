06f4f369efd381a6765f489932ade81a86bf2e38
Julien Lenne <contact.lenne@gmail.com>
2020-11-25 13:04:50 +0800

    change useLayoutEffect
    change useLayoutEffect to useEffect to be fully compatible with next.js

--- src/components/common/Interactive.tsx
+++ src/components/common/Interactive.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useLayoutEffect, useRef, useCallback } from "react";
+import React, { useState, useEffect, useRef, useCallback } from "react";
 
 import { useEventCallback } from "../../hooks/useEventCallback";
 import { clamp } from "../../utils/clamp";
@@ -98,7 +98,7 @@ const InteractiveBase = ({ onMove, onKey, ...rest }: Props) => {
     [handleMove, handleMoveEnd]
   );
 
-  useLayoutEffect(() => {
+  useEffect(() => {
     toggleDocumentEvents(isDragging);
     return () => {
       isDragging && toggleDocumentEvents(false);
