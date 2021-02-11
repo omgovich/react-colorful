aed96f7b7665ade19221765a098c0a42e18f84af
Vlad Shilov <omgovich@ya.ru>
2021-02-11 19:18:29 +0300

    Describe new CSS loading technique

--- src/hooks/useStyleSheet.ts
+++ src/hooks/useStyleSheet.ts
@@ -1,16 +1,18 @@
 import { useIsomorphicLayoutEffect } from "./useIsomorphicLayoutEffect";
+
+// Bundler is configured to load this as a processed minified CSS-string
 import styles from "../css/styles.css";
 
-let sheet: HTMLStyleElement | null;
+let styleElement: HTMLStyleElement | undefined;
 
 /**
  * Injects CSS code into the document's <head>
  */
 export const useStyleSheet = (): void => {
   useIsomorphicLayoutEffect(() => {
-    if (typeof document !== "undefined" && !sheet) {
-      sheet = document.head.appendChild(document.createElement("style"));
-      sheet.innerHTML = styles;
+    if (typeof document !== "undefined" && !styleElement) {
+      styleElement = document.head.appendChild(document.createElement("style"));
+      styleElement.innerHTML = styles;
     }
   }, []);
 };
