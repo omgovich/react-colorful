096d2ad1c31bb82e10fe90d3c8575954cd96ab6e
Vlad Shilov <omgovich@ya.ru>
2021-03-25 12:08:39 +0300

    CSP: Support `nonce`

--- src/index.ts
+++ src/index.ts
@@ -18,3 +18,6 @@ export { HexColorInput } from "./components/HexColorInput";
 
 // Color model types
 export { RgbColor, RgbaColor, HslColor, HslaColor, HsvColor, HsvaColor } from "./types";
+
+// Tooling
+export { setNonce } from "./utils/nonce";
