f3a899f2c54834f42818bb553d0d8458a2b21699
Vlad Shilov <omgovich@ya.ru>
2021-09-24 10:01:16 +0300

    Better a11y: Use `grid` role in `Saturation` component

--- src/components/common/Interactive.tsx
+++ src/components/common/Interactive.tsx
@@ -36,7 +36,7 @@ const isInvalid = (event: MouseEvent | TouchEvent, hasTouch: boolean): boolean =
   return hasTouch && !isTouch(event);
 };
 
-interface Props {
+interface Props extends React.HTMLAttributes<HTMLDivElement> {
   onMove: (interaction: Interaction) => void;
   onKey: (offset: Interaction) => void;
   children: React.ReactNode;
@@ -118,7 +118,6 @@ const InteractiveBase = ({ onMove, onKey, ...rest }: Props) => {
 
   return (
     <div
-      {...rest}
       onTouchStart={handleMoveStart}
       onMouseDown={handleMoveStart}
       className="react-colorful__interactive"
@@ -126,6 +125,7 @@ const InteractiveBase = ({ onMove, onKey, ...rest }: Props) => {
       onKeyDown={handleKeyDown}
       tabIndex={0}
       role="slider"
+      {...rest}
     />
   );
 };
