12d86c562e59bc30dc401ea51ec2fab1ac9b38de
RoyEden <roy.ede@gmail.com>
2022-06-05 17:39:13 -0300

    Use `substring` instead of deprecated `substr`.

--- src/utils/convert.ts
+++ src/utils/convert.ts
@@ -14,7 +14,7 @@ const angleUnits: Record<string, number> = {
 export const hexToHsva = (hex: string): HsvaColor => rgbaToHsva(hexToRgba(hex));
 
 export const hexToRgba = (hex: string): RgbaColor => {
-  if (hex[0] === "#") hex = hex.substr(1);
+  if (hex[0] === "#") hex = hex.substring(1);
 
   if (hex.length < 6) {
     return {
@@ -26,9 +26,9 @@ export const hexToRgba = (hex: string): RgbaColor => {
   }
 
   return {
-    r: parseInt(hex.substr(0, 2), 16),
-    g: parseInt(hex.substr(2, 2), 16),
-    b: parseInt(hex.substr(4, 2), 16),
+    r: parseInt(hex.substring(0, 2), 16),
+    g: parseInt(hex.substring(2, 4), 16),
+    b: parseInt(hex.substring(4, 6), 16),
     a: 1,
   };
 };
